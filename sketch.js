ea9df18c30a747b09987c4ca7ef80cbf20e4ca3d
JoaquUIN <96552283+JoaquUIN@users.noreply.github.com>
2021-12-22 17:14:42 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+
+
+
+function draw() {
+   background("white");
+  text("Lives: " + life,200,100);
+  strokeWeight(0);
+  fill("lightblue");
+  rect(0,120,52,140);
+  fill("yellow");
+  rect(345,120,52,140);
+  
+
+
+ 
+  
+  
+  
+ 
+}
